18c8c59052a223fc5c1f77b4c3b64ea26118dc33
chen-g60609111 <b08202022@ntu.edu.tw>
2021-07-24 19:49:10 +0800

    start

---
+++ classmanagement.py
@@ -0,0 +1,17 @@
+
+run = 1
+while(run == 1):
+    print("1:已修課程; 2:未修課程; 3:輸入資料; 4:計算學分; 5:結束")
+    mode = input("請輸入模式:")
+    if(mode == "1"):
+        print("已修課程")
+    elif(mode == "2"):
+        print("未修課程")
+    elif(mode == "3"):
+        print("輸入資料")
+    elif(mode == "4"):
+        print("計算學分")
+    elif(mode == "5"):
+        print("結束")
+    else:
+        continue
