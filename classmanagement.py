05436f562c1ddffdcb37b2a63763c0bcc8daf205
chen-g60609111 <b08202022@ntu.edu.tw>
2021-08-04 00:52:57 +0800

    up

--- classmanagement.py
+++ classmanagement.py
@@ -1,13 +1,23 @@
 import csv
+import os
+
+filepath = "./output.csv"
+if os.path.isfile(filepath):
+    print("檔案存在。")
+else:
+    print("檔案不存在。建立")
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["學年", "性質", '課名', "學分", '成績', '排名'])
 
 class ClassData:
     def __init__(self,semester, kind, name,credit ,score ,rank):
-        self.semester = semester
-        self.kind = kind
-        self.name = name
-        self.credit = credit
-        self.score = score
-        self.rank = rank
+        self.semester = semester   #學期
+        self.kind = kind           #課程分類
+        self.name = name           #課名
+        self.credit = credit       #學分
+        self.score = score         #分數
+        self.rank = rank           #排名
     def turnlist(self):
         datalist = [self.semester,self.kind,self.name,self.credit,self.score,self.rank]
         return datalist
@@ -19,6 +29,13 @@ while(run == 1):
     mode = input("請輸入模式:")
     while(mode == "1"):
         print("已修課程")
+        with open('output.csv', newline='') as csvfile:
+            # 讀取 CSV 檔案內容
+            rows = csv.reader(csvfile)
+            for row in rows:
+                #row = list(rows)
+                print(row[1][1])
+            
         mode = "0"
         
     while(mode == "2"):
@@ -27,11 +44,11 @@ while(run == 1):
     while(mode == "3"):
         print("輸入資料")
         print("學年 性質 課名 學分 成績 排名:")
-        #data = input().split(" ")
+        data = input().split(" ")
         with open('output.csv', 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(["學年", "性質", '課名', "學分", '成績', '排名'])
-            writer.writerow(class1.turnlist())
+            writer.writerow(data)
+            #writer.writerow(class1.turnlist())
         #print(data)
         mode = "0"
     while(mode == "4"):
--- test.py
+++ test.py
@@ -1,17 +1,15 @@
 #this is a modal
-class ClassData:
-    def __init__(self,semester, kind, name,credit ,score ,rank):
-        self.semester = semester
-        self.kind = kind
-        self.name = name
-        self.credit = credit
-        self.score = score
-        self.rank = rank
-    def turnlist(self):
-        datalist = [self.semester,self.kind,self.name,self.credit,self.score,self.rank]
-        return datalist
-    
-class1 = ClassData("109-1","通識","哈哈",3,"A+","5%")
-print(type(class1.turnlist()))
-print(class1.name)
+import os
+import csv
+# 要檢查的檔案路徑
+filepath = "./output.csv"
 
+# 檢查檔案是否存在
+if os.path.isfile(filepath):
+    print("檔案存在。")
+  
+else:
+    print("檔案不存在。")
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["學年", "性質", '課名', "學分", '成績', '排名'])
